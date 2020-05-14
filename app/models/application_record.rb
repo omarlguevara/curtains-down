f61d1fe4dad2edee75ebf5e22cd1fd2ff2ac0b41
omarlguevara <omarlguevara78@gmail.com>
2020-05-14 11:42:48 -0400

    revamp

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  config.cache_classes = true
-  config.serve_static_assets = true
-  config.assets.compile = true
-  config.assets.digest = true
+  
 end
