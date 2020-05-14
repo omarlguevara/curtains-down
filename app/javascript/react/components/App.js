c5520d31018ffa48d406df147e9348ab9b4a0e1d
omarlguevara <omarlguevara78@gmail.com>
2020-05-14 11:41:30 -0400

    added classes into the ApplicationRecord

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
+
 import TheatersIndexContainer from "../containers/TheatersIndexContainer"
 import TheatersShowContainer from "../containers/TheatersShowContainer"
 import UserShowContainer from "../containers/UserShowContainer"
+console.log(process.env.REACT_APP_GOOGLE_API_KEY)
 
 const App = (props) => {
   return (
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
 end
